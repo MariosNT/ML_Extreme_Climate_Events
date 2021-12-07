5f8fd00ed17b5e5a4d6f92ca4675cffd0f1df13b
Ritabrata Dutta <rito@itadmins-MacBook-Air.local>
2021-12-07 15:50:06 +0000

    corrections to timeseries_v3.py and addition of extreme timeseries v1

--- code/timeseries_v3.py
+++ code/timeseries_v3.py
@@ -7,7 +7,7 @@ from Sampler import EllipticalSliceSampling
 import sys
 
 #X = np.random.normal(size=(100, 5), loc=1, scale=1) # Returns a 100x2 matrix of random (normal) elements
-
+# Model fields
 X = np.load('../Data/Data/model_fields_Cardiff.npy')
 # Calculating transpose such that each row corresponds for a day
 X = np.transpose(X)
@@ -16,6 +16,10 @@ X = np.concatenate((X[:,[0,3,4,5]],np.sqrt(pow(X[:,1],2)+pow(X[:,2],2)).reshape(
 # Standardize data (making each column having 0 mean and stdev 1)
 X -= np.mean(X, axis=0)
 X /= np.std(X, axis=0)
+
+# Rain fall
+Y = np.load('../Data/Data/rainfall_Cardiff_1979.npy')
+print(Y.shape)
 
 class cptimeseries():
     def __init__(self, theta, k=6, p=5):
@@ -159,7 +163,6 @@ print(cptimeseries(true_theta).loglikelihood(z, y, X))
 
 
 #### Now we want to implment a Gibbs sample where we update theta and z one after another
-""" Not a 100% sure about this part - need to recheck """
 
 # number of steps Gibbs we want to use
 n_step_Gibbs = 100
@@ -168,44 +171,55 @@ n_step_Gibbs = 100
 Theta, Z = [], []
 
 # Extract zero/non-zero indices of y
-zero_y_indices = [i for i, e in enumerate(y) if e == 0] #"Maybe faster with numpy boolean, but maybe this way useful for later"
-nonzero_y_indices = [i for i, e in enumerate(y) if e != 0]
+zero_y_indices = [i for i, e in enumerate(Y) if e == 0] #"Maybe faster with numpy boolean, but maybe this way useful for later"
+nonzero_y_indices = [i for i, e in enumerate(Y) if e != 0]
 
 ## Lets first initialize theta and z for a Markov chain ##
-z_state = np.ones(shape=y.shape)
+z_state = np.ones(shape=Y.shape)
 z_state[zero_y_indices] = 0 #z_state an array of 0, 1
 theta_state = theta_0
+# Add to stored samples
+Theta.append(copy.deepcopy(theta_state))
+Z.append(copy.deepcopy(z_state))
 
 for ind_Gibbs in range(n_step_Gibbs):
     #print(ind_Gibbs)
-
-    #### First sample theta using Elliptic Slice Sampler ###
-    # define conditional likelihood for theta
-    loglikelihood_theta = lambda theta: cptimeseries(theta).loglikelihood(z_state, y, X)
-
-    # Sample/Update theta
-    ## Here Mean and Sigma are the mean and var-cov matrix of Multivariate normal used as the prior.
-    ## f_0 defines the present state of the Markov chain
-    Samples = EllipticalSliceSampling(LHD=loglikelihood_theta, n = 1, Mean=theta_0, Sigma=Sigma_0, f_0=theta_state)
-    theta_state = Samples[-1]
+    theta_state = copy.deepcopy(Theta[-1])
+    z_state = copy.deepcopy(Z[-1])
+    while True:
+        try:
+            #### First sample theta using Elliptic Slice Sampler ###
+            # define conditional likelihood for theta
+            loglikelihood_theta = lambda theta: cptimeseries(theta).loglikelihood(z_state, Y, X)
+            # Sample/Update theta
+            ## Here Mean and Sigma are the mean and var-cov matrix of Multivariate normal used as the prior.
+            ## f_0 defines the present state of the Markov chain
+            Samples = EllipticalSliceSampling(LHD=loglikelihood_theta, n=1, Mean=theta_0, Sigma=Sigma_0,
+                                              f_0=theta_state)
+            theta_state = Samples[-1]
+            # define conditional likelihood for z
+            loglikelihood_z = lambda z: cptimeseries(theta_state).loglikelihood(z, Y, X)
+            # Sample/Update z
+            possible_z = z_state
+            for ind_nonzero in nonzero_y_indices:
+                prob_z = np.zeros(9)
+                for ind_z in range(9):  ### Why 9 here? Times it rains/day
+                    possible_z[ind_nonzero] = ind_z + 1
+                    prob_z[ind_z] = loglikelihood_z(possible_z)
+                #print(prob_z)
+                finite_indices = np.isfinite(prob_z)
+                prob_z = np.exp(prob_z[finite_indices] - np.min(prob_z[finite_indices]))
+                possible_z[ind_nonzero] = np.random.choice(a=np.arange(1, 10)[finite_indices],
+                                                           p=prob_z / np.sum(prob_z))
+            z_state = possible_z
+        except (RuntimeError, ValueError, TypeError, NameError, ZeroDivisionError, OSError):
+            continue
+        break
+    print(str(ind_Gibbs)+'-st/th iteration successfully finished' )
+    # Add to stored samples
     Theta.append(copy.deepcopy(theta_state))
-
-    # define conditional likelihood for z
-    loglikelihood_z = lambda z: cptimeseries(theta_state).loglikelihood(z, y, X)
-    # Sample/Update z
-    possible_z = z_state
-    for ind_nonzero in nonzero_y_indices:
-        prob_z = np.zeros(9)
-        for ind_z in range(9): ### Why 9 here? Times it rains/day
-            possible_z[ind_nonzero] = ind_z + 1
-            prob_z[ind_z] = loglikelihood_z(possible_z)
-        #print(prob_z)
-        finite_indices = np.isfinite(prob_z)
-        prob_z = np.exp(prob_z[finite_indices] - np.min(prob_z[finite_indices]))
-        possible_z[ind_nonzero] = np.random.choice(a=np.arange(1, 10)[finite_indices], p=prob_z/np.sum(prob_z))
-    z_state = possible_z
     Z.append(copy.deepcopy(z_state))
-    print(str(ind_Gibbs)+'-th sample LogLikeliHood: '+str(cptimeseries(Theta[ind_Gibbs]).loglikelihood(Z[ind_Gibbs],y, X)))
+    print(str(ind_Gibbs)+'-st/th sample LogLikeliHood: '+str(cptimeseries(Theta[ind_Gibbs]).loglikelihood(Z[ind_Gibbs],Y, X)))
 
 
 
